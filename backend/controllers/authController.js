c70086c36090c98070e4d756f73f5609a0bceffb
mfurqaann <furqan1061@gmail.com>
2025-05-21 09:32:27 +0700

    refactor(onlinestore): refactor backend controller & route

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -14,10 +14,8 @@ export const register = async (req, res) => {
 
         const hashedPassword = await bcrypt.hash(password, 10);
         const userId = await createUser(name, email, hashedPassword);
-        const user = { id: userId, name, email };
-        const token = generateToken(userId)
-        sendToken(user, res, token)
 
+        res.status(201).json({ message: 'User registered successfully!' })
     } catch (error) {
         res.status(500).json({ message: error })
     }
